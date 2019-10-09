650821a1c6ce2f34453bb1a0e35f4b161e94fe81
Kwan Kim <gimugani@gmail.com>
2019-10-09 10:14:56 -0700

    connect database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const profile = require('./controllers/profile');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-parallel-84562',
       user : 'kwankim',
       password : '',
       database : 'smart-brain'
